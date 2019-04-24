71b1fa3184b212516508074f160858cb2f308c44
stomg7969 <stomg7969@gmail.com>
2019-04-24 15:02:37 -0400

    Updated wrong img url

--- moonya-backend/db/seeds.rb
+++ moonya-backend/db/seeds.rb
@@ -10,7 +10,7 @@
 User.destroy_all
 User.create(username: 'admin', email: 'admin', password: 'admin', isAdmin: true)
 
-# Product attributes => name, price, inventory, category, color, gender, material:[], sizes:[]
+# Product attributes => name, price, inventory, category, color, gender, material:[], sizes:[].
 Product.destroy_all
 Product.create(name: 'Boy Snug-fit White Dino Innerwear', price: 49.00, imgFront: 'https://dl.airtable.com/ul74gey2TbGJhBxEgzXE_full_%EC%A0%9C%EB%AA%A9%20%EC%97%86%EB%8A%94%20%EC%B9%B4%ED%83%88%EB%A1%9D%2000872_resized.jpg', imgBack: 'https://dl.airtable.com/8GEBjtWQgOE8A2YdYoa1_full_MCCBBMAA07.jpg', inventory: 30, category: 'innerwear', color: 'white', gender: 'boy', material: ['modal', 'cotton', 'spandex'], sizes:['small', 'medium', 'large'])
 Product.create(name: 'Boy Snug-fit Black Check Innerwear', price: 59.00, imgFront: 'https://dl.airtable.com/riiX9593TyKCTAEAwPzt_full_%EC%A0%9C%EB%AA%A9%20%EC%97%86%EB%8A%94%20%EC%B9%B4%ED%83%88%EB%A1%9D%2000906_resized.jpg', imgBack: 'https://dl.airtable.com/aGI38YrlTJ2DgDU4oiRI_full_MCCBBMAA11.jpg', inventory: 30, category: 'innerwear', color: 'black', gender: 'boy', material: ['modal', 'cotton', 'spandex'], sizes:['small', 'medium', 'large'])
@@ -32,7 +32,7 @@ Product.create(name: 'Girl Light Blue Plain Pajams', price: 75.00, imgFront: 'ht
 
 Product.create(name: 'Girl Pink Flower Pajamas', price: 79.00, imgFront: 'https://dl.airtable.com/SsdOyF4IQwOg3lKjkiBP_full_mny_outdoor_070.jpg', imgBack: 'https://dl.airtable.com/Qov06u6QwS3IW35n9jr9_full_MCCBBFEA05.jpg', inventory: 30, category: 'pajamas', color: 'pink', gender: 'girl', material: ['cotton'], sizes:['small', 'medium', 'large'])
 Product.create(name: 'Navy Star Robe Pajamas', price: 99.00, imgFront: 'https://dl.airtable.com/SIhTsWH5SQupkelOEj8K_full_%EC%A0%9C%EB%AA%A9%20%EC%97%86%EB%8A%94%20%EC%B9%B4%ED%83%88%EB%A1%9D%2000645_resized.jpg', imgBack: 'https://dl.airtable.com/ODjh7akoTASfLN6k3YPx_full_MCCBBAEA01.jpg', inventory: 30, category: 'pajamas', color: 'Navy', gender: 'unisex', material: ['modal', 'spandex'], sizes:['small', 'medium', 'large'])
-Product.create(name: 'Girl Pink Red-dot Dress', price: 89.00, imgFront: 'https://dl.airtable.com/KUzSAWRXROSzClvqy1RP_full_20180723GBStyle5749.jpg', imgBack: 'https://dl.airtable.com/iojuhJJ1Rx2xp2v5O98r_full_MCCBBFED13.jpg', inventory: 30, category: 'dress', color: 'pink', gender: 'girl', material: ['cotton', 'spandex'], sizes:['small', 'medium', 'large'])
+Product.create(name: 'Girl Pink Red-dot Dress', price: 89.00, imgFront: 'https://dl.airtable.com/.attachmentThumbnails/ebfbc4c490d708d1480b395b32fe00c4/dc3b9e4b', imgBack: 'https://dl.airtable.com/iojuhJJ1Rx2xp2v5O98r_full_MCCBBFED13.jpg', inventory: 30, category: 'dress', color: 'pink', gender: 'girl', material: ['cotton', 'spandex'], sizes:['small', 'medium', 'large'])
 Product.create(name: 'Red Circle Sweater', price: 17.00, imgFront: 'https://dl.airtable.com/ln4wmfBTRJqtjkW1YNVt_full_20180619GBstyle153484.jpg', imgBack: 'https://dl.airtable.com/6X1DAXtiQJWuJAkK7p7X_full_CDCBBACD01.jpg', inventory: 30, category: 'sweater', color: 'red', gender: 'unisex', material: ['modal', 'cotton'], sizes:['small', 'medium', 'large'])
 Product.create(name: 'Yellow Circle Sweater', price: 17.00, imgFront: 'https://dl.airtable.com/HiFTMurUSpmWwSjCFyXk_full_20180618GBstyle150779.jpg', imgBack: 'https://dl.airtable.com/QiyCLT5SR6WYEzgCB1nQ_full_CDCBBACD03.jpg', inventory: 30, category: 'sweater', color: 'yellow', gender: 'unisex', material: ['modal', 'cotton'], sizes:['small', 'medium', 'large'])
 
